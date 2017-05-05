08e49c0aa84b85ba516447e28b02478a2793ad40
h3poteto <h3.poteto@gmail.com>
2017-05-05 23:57:28 +0900

    refs #18 Fix specs

--- spec/requests/inquiries_spec.rb
+++ spec/requests/inquiries_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Inquiries", :type => :request do
-  describe "GET /inquiries" do
+  describe "GET /inquiry/new" do
     it "works! (now write some real specs)" do
-      get inquiries_path
+      get new_inquiry_path
       expect(response).to have_http_status(200)
     end
   end
