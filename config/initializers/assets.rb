ee1e74943467a73b3b5bafc9f9db14f3cb77727a
AkiraFukushima <h3.poteto@gmail.com>
2018-03-28 18:43:18 +0900

    refs #33 Change layout for ios page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = "1.0"
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w[*.js app/assets/*.css app/assets/*.less *.erb smart_phone.css]
+Rails.application.config.assets.precompile += %w[*.js app/assets/*.css app/assets/*.less *.erb ios.css]
