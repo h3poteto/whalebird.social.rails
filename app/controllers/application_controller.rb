5f2fd9413a12150f29fd9e3d69b57be5a5088d4d
AkiraFukushima <h3.poteto@gmail.com>
2018-03-29 11:09:09 +0900

    refs #38 Auto detect languages from accept language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :set_locale
+  before_action :redirect_locale
+  before_action :set_locale
 
+  helper_method :i18n_url_for
+
+  # パラメータでlocale指定があった場合にはそれを優先する
   def set_locale
     I18n.locale = locale
   end
@@ -17,5 +21,24 @@ class ApplicationController < ActionController::Base
 
   def default_url_options(options = {})
     options.merge(locale: locale)
+  end
+
+  # パラメータでlocaleの指定がない場合はaccept_languageに従う
+  def redirect_locale
+    return unless params[:locale].nil?
+    locale = locale_in_accept_language
+    redirect_to i18n_url_for(params.merge(locale: locale.to_s)) if locale.present?
+  end
+
+  def locale_in_accept_language
+    request.env['HTTP_ACCEPT_LANGUAGE']
+      .to_s.split(',')
+      .map{ |_| _[0..1].to_sym }
+      .select { |_| I18n::available_locales.include?(_) }
+      .first
+  end
+
+  def i18n_url_for(options)
+    url_for(options)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,2 @@
 module ApplicationHelper
-  def i18n_url_for(options)
-    options[:locale] = nil if options[:locale] == I18n.default_locale
-    url_for(options)
-  end
 end
