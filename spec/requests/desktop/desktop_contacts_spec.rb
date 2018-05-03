fb6b6068d194a0e33b8b843f0c5426935f3642e1
AkiraFukushima <h3.poteto@gmail.com>
2018-05-03 10:27:00 +0900

    refs #45 Fix contacts tests

--- spec/requests/desktop/desktop_contacts_spec.rb
+++ spec/requests/desktop/desktop_contacts_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Desktop::Contacts", type: :request do
-  describe "GET /desktop_contacts" do
+  describe "GET /desktop/contacts/new" do
     it "works! (now write some real specs)" do
-      get desktop_contacts_path
+      get new_desktop_contact_path
       expect(response).to have_http_status(200)
     end
   end
--- spec/routing/desktop/contacts_routing_spec.rb
+++ spec/routing/desktop/contacts_routing_spec.rb
@@ -2,11 +2,6 @@ require "rails_helper"
 
 RSpec.describe Desktop::ContactsController, type: :routing do
   describe "routing" do
-
-    it "routes to #index" do
-      expect(:get => "/desktop/contacts").to route_to("desktop/contacts#index")
-    end
-
     it "routes to #new" do
       expect(:get => "/desktop/contacts/new").to route_to("desktop/contacts#new")
     end
@@ -15,25 +10,8 @@ RSpec.describe Desktop::ContactsController, type: :routing do
       expect(:get => "/desktop/contacts/1").to route_to("desktop/contacts#show", :id => "1")
     end
 
-    it "routes to #edit" do
-      expect(:get => "/desktop/contacts/1/edit").to route_to("desktop/contacts#edit", :id => "1")
-    end
-
     it "routes to #create" do
       expect(:post => "/desktop/contacts").to route_to("desktop/contacts#create")
     end
-
-    it "routes to #update via PUT" do
-      expect(:put => "/desktop/contacts/1").to route_to("desktop/contacts#update", :id => "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(:patch => "/desktop/contacts/1").to route_to("desktop/contacts#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/desktop/contacts/1").to route_to("desktop/contacts#destroy", :id => "1")
-    end
-
   end
 end
