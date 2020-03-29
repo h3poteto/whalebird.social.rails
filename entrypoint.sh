babbe6024f85e52812946dc3d78a0c552d4ffa86
AkiraFukushima <h3.poteto@gmail.com>
2020-03-29 18:49:11 +0900

    Remove ssm call in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
 
-export AWS_DEFAULT_REGION=ap-northeast-1
-
-myaws ssm parameter get whalebirdorg.$SERVICE_ENV.db_host --region $AWS_DEFAULT_REGION
-ret=$?
-if [[ $ret -ne 0 ]]; then
-    exit
-fi
-
-export DB_HOST=`myaws ssm parameter get whalebirdorg.$SERVICE_ENV.db_host --region $AWS_DEFAULT_REGION`
-export DB_USERNAME=`myaws ssm parameter get whalebirdorg.$SERVICE_ENV.db_username --region $AWS_DEFAULT_REGION`
-export DB_PASSWORD=`myaws ssm parameter get whalebirdorg.$SERVICE_ENV.db_password --region $AWS_DEFAULT_REGION`
-export SECRET_KEY_BASE=`myaws ssm parameter get whalebirdorg.$SERVICE_ENV.secret_key_base --region $AWS_DEFAULT_REGION`
-export SERVER_EMAIL=`myaws ssm parameter get whalebirdorg.$SERVICE_ENV.server_email --region $AWS_DEFAULT_REGION`
-export SERVER_EMAIL_PASSWORD=`myaws ssm parameter get whalebirdorg.$SERVICE_ENV.server_email_password --region $AWS_DEFAULT_REGION`
-export WEB_EMAIL=`myaws ssm parameter get whalebirdorg.$SERVICE_ENV.web_email --region $AWS_DEFAULT_REGION`
-export SLACK_WEBHOOK_URL=`myaws ssm parameter get whalebirdorg.$SERVICE_ENV.slack_webhook_url --region $AWS_DEFAULT_REGION`
-export RAILS_SERVE_STATIC_FILES=true
-export RAILS_LOG_TO_STDOUT=true
-export PORT=3000
-
 exec "$@"
