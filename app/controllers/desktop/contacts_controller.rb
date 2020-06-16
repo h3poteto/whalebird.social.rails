5511b82d25c5fc7f1a87aff2006ca03467fdf50a
AkiraFukushima <h3.poteto@gmail.com>
2020-06-17 00:33:36 +0900

    Introduce reCAPTCHA for contact form

--- app/controllers/desktop/contacts_controller.rb
+++ app/controllers/desktop/contacts_controller.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
+require 'net/http'
+require 'uri'
+
+class RecaptchaError < StandardError
+  def initialize(msg)
+    super
+  end
+end
 
 class Desktop::ContactsController < DesktopController
+  RECAPTCHA_SITEVERIFY_URL = 'https://www.google.com/recaptcha/api/siteverify'
 
   # GET /desktop/contacts/1
   def show
@@ -16,7 +25,24 @@ class Desktop::ContactsController < DesktopController
 
   # POST /desktop/contacts
   def create
+    res = Net::HTTP.post_form(
+      URI.parse(RECAPTCHA_SITEVERIFY_URL),
+      {
+        'secret' => ENV['RECAPTCHA_SECRET_KEY'],
+        'response' => params[:recaptcha_response]
+      })
+    # failed: "{\n  \"success\": false,\n  \"error-codes\": [\n    \"missing-input-response\"\n  ]\n}"
+    # success: "{\n  \"success\": true,\n  \"challenge_ts\": \"2020-06-16T14:17:18Z\",\n  \"hostname\": \"localhost\",\n  \"score\": 0.9,\n  \"action\": \"contact\"\n}"
+    response = JSON.parse(res.body)
+    unless response["success"]
+      raise RecaptchaError, response["error-codes"]
+    end
+
     @inquiry = ::Inquiry.new(inquiry_params)
+    unless response["score"] > 0.5
+      flash[:notice] = t('desktop.contacts.create.recaptcha_score_error')
+      return render :new
+    end
 
     if @inquiry.save
       session[:inquiry_id] = @inquiry.id
@@ -28,6 +54,6 @@ class Desktop::ContactsController < DesktopController
 
   private
     def inquiry_params
-      params.require(:inquiry).permit(:email, :body)
+      params.require(:inquiry).permit(:email, :body, :recaptcha_response)
     end
 end
