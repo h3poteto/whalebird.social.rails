0526d715a25e2673cf135a00bc11cdac1a0719d1
AkiraFukushima <h3.poteto@gmail.com>
2023-02-20 00:56:27 +0900

    Fix spec

--- spec/requests/desktop/desktop_contacts_spec.rb
+++ spec/requests/desktop/desktop_contacts_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Desktop::Contacts", type: :request do
   describe "GET /desktop/contacts/new" do
     it "works! (now write some real specs)" do
-      get new_desktop_contact_path
+      get new_desktop_contacts_path
       expect(response).to have_http_status(200)
     end
   end
