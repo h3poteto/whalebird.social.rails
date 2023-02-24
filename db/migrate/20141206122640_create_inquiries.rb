6eb60c510e538193369f26ad5bd28d23865d99df
AkiraFukushima <h3.poteto@gmail.com>
2023-02-24 23:35:01 +0900

    refs #592 Change database to sqlite3

--- db/migrate/20141206122640_create_inquiries.rb
+++
@@ -1,9 +0,0 @@
-class CreateInquiries < ActiveRecord::Migration[4.2]
-  def change
-    create_table :inquiries do |t|
-      t.string :email
-      t.text   :body
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20190828161840_remove_helps.rb
+++
@@ -1,5 +0,0 @@
-class RemoveHelps < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :helps
-  end
-end
